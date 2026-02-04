936f953ba6968c0b77ed5a414c25c33f7af970af
Shreyaan <shre6745@gmail.com>
2026-02-04 18:19:47 +0530

    project pushed to github

---
+++ client/src/components/loader/FallbackLoading.jsx
@@ -0,0 +1,12 @@
+
+import { PulseLoader } from "react-spinners";
+
+const FallbackLoading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <PulseLoader color="#008cff" />
+    </div>
+  );
+};
+
+export default FallbackLoading;
